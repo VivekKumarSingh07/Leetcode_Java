5d5c48fbee88d3597f4ef73ae0ee71279b8c0973
Alvaro Falcon Morales <alvaro@2coders.com>
2019-10-14 23:50:02 +0100

    414. Third Maximum Number

---
+++ Algorithms/414. Third Maximum Number.java
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/third-maximum-number/
+
+/**
+ * Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+ *
+ * Example 1:
+ *
+ * Input: [3, 2, 1]
+ *
+ * Output: 1
+ *
+ * Explanation: The third maximum is 1.
+ *
+ * Example 2:
+ *
+ * Input: [1, 2]
+ *
+ * Output: 2
+ *
+ * Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+ *
+ * Example 3:
+ *
+ * Input: [2, 2, 3, 1]
+ *
+ * Output: 1
+ *
+ * Explanation: Note that the third maximum here means the third maximum distinct number.
+ * Both numbers with value 2 are both considered as second maximum.
+ */
+public class Solution {
+
+    public int thirdMax(int[] nums) {
+        ArrayList<Integer> numList = (ArrayList<Integer>) Arrays.stream(nums).boxed().collect(Collectors.toList());
+        if(numList.size() < 3) return Collections.max(numList);
+        int maxNum = Collections.max(numList);
+        int removed = 0;
+        while (removed < 2){
+            if (numList.isEmpty()) return maxNum;
+            Integer max = Collections.max(numList);
+            while(numList.indexOf(max) != -1){
+                numList.remove(numList.indexOf(max));
+            }
+            removed++;
+        }
+        return numList.isEmpty() ? maxNum : Collections.max(numList);
+    }
+}
